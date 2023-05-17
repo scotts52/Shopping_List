8c7e803dfc879cd1425ebcb5cab71ca098a82238
scotts52 <scotty.stevenson123@gmail.com>
2023-05-17 14:06:06 +0100

    first commit

---
+++ Shopping_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shopping_ListApp.swift
+//  Shopping List
+//
+//  Created by Stevenson, Scott on 17/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Shopping_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
